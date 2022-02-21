813cd0d9fdc5fa1494feb0d0b9ff06b04d661704
Kun Liu <liuku@microsoft.com>
2022-02-21 16:27:06 +0800

    Add SimpleProcessEvent

---
+++ azure-eventhubs-test/src/main/java/com/azure/eventhubs/scenarios/SimpleProcessEvent.java
@@ -0,0 +1,49 @@
+package com.azure.eventhubs.scenarios;
+
+import com.azure.eventhubs.util.Credentials;
+import com.azure.messaging.eventhubs.EventHubClientBuilder;
+import com.azure.messaging.eventhubs.EventProcessorClient;
+import com.azure.messaging.eventhubs.EventProcessorClientBuilder;
+import com.azure.messaging.eventhubs.checkpointstore.blob.BlobCheckpointStore;
+import com.azure.storage.blob.BlobContainerAsyncClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service("SimpleProcessEvent")
+public class SimpleProcessEvent extends EventHubsScenario{
+    @Override
+    public void run() {
+
+        BlobContainerAsyncClient blobContainerAsyncClient = new BlobContainerClientBuilder()
+                .connectionString(Credentials.storageConnectionString)
+                .containerName(Credentials.storageContainer)
+                .buildAsyncClient();
+
+        EventProcessorClient eventProcessorClient = new EventProcessorClientBuilder()
+                .consumerGroup(EventHubClientBuilder.DEFAULT_CONSUMER_GROUP_NAME)
+                .connectionString(Credentials.eventHubsConnectionString, Credentials.eventHub)
+                .checkpointStore(new BlobCheckpointStore(blobContainerAsyncClient))
+                .processEvent(eventContext -> {
+                    System.out.println("Partition id = " + eventContext.getPartitionContext().getPartitionId() + " and "
+                            + "sequence number of event = " + eventContext.getEventData().getSequenceNumber());
+                })
+                .processError(errorContext -> {
+                    System.out
+                            .println("Error occurred while processing events " + errorContext.getThrowable().getMessage());
+                })
+                .buildEventProcessorClient();
+
+        eventProcessorClient.start();
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        eventProcessorClient.stop();
+    }
+}
