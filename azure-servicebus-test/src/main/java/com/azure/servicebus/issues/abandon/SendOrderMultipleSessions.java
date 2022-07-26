e14557991e535ee9ba7af68c5a1a9f86e5e493e5
Kun Liu <liuku@microsoft.com>
2022-07-26 14:15:36 +0800

    Update test for session processor issue

--- azure-servicebus-test/src/main/java/com/azure/servicebus/issues/abandon/SendOrderMultipleSessions.java
+++ azure-servicebus-test/src/main/java/com/azure/servicebus/issues/abandon/SendOrderMultipleSessions.java
@@ -9,8 +9,9 @@ import com.azure.servicebus.util.Credentials;
 import java.time.Duration;
 import java.util.List;
 
-public class SendOrderTwoSessions {
+public class SendOrderMultipleSessions {
     private static final int SEND_MESSAGE_NUMBER = 10;
+    private static final int SESSION_NUMBER = 3;
 
     public static void main(String[] args) {
         AmqpRetryOptions options = new AmqpRetryOptions();
@@ -51,7 +52,19 @@ public class SendOrderTwoSessions {
                 e.printStackTrace();
             }
         }
+        for(int i = 0; i < SEND_MESSAGE_NUMBER; i++){
+            List<ServiceBusMessage> messages = List.of(
+                    new ServiceBusMessage("" + i).setMessageId("" + i).setSessionId("test2"));
 
+            sender.sendMessages(messages);
+
+            System.out.println("Send Message id: " + i);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         sender.close();
     }
 }
