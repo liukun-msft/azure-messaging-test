0bbfa70d4bac1e7d5f9377d831bb7b5712981c7f
Kun Liu <liuku@microsoft.com>
2022-08-08 13:54:22 +0800

    Add schedule message

---
+++ azure-servicebus-test/src/main/java/com/azure/servicebus/common/ScheduleMessage.java
@@ -0,0 +1,45 @@
+package com.azure.servicebus.common;
+
+import com.azure.core.amqp.AmqpRetryOptions;
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
+import com.azure.servicebus.util.Credentials;
+
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+public class ScheduleMessage {
+    private static final int SEND_MESSAGE_NUMBER = 10;
+    private static final int SCHEDULE_GAP_IN_SECONDS = 30;
+
+    public static void main(String[] args) {
+        AmqpRetryOptions options = new AmqpRetryOptions();
+        options.setTryTimeout(Duration.ofMinutes(1L));
+
+        ServiceBusSenderClient sender = new ServiceBusClientBuilder()
+                .connectionString(Credentials.serviceBusConnectionString)
+                .retryOptions(options)
+                .sender()
+                .queueName(Credentials.serviceBusQueue)
+                .buildClient();
+
+
+        for(int i = 0; i < SEND_MESSAGE_NUMBER; i++){
+            ServiceBusMessage messages = new ServiceBusMessage("Hello world").setMessageId("" + i);
+
+            OffsetDateTime scheduleTime = OffsetDateTime.now().plusSeconds(30);
+            sender.scheduleMessage(messages, scheduleTime);
+
+            System.out.println("Send Message id: " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        sender.close();
+    }
+}
