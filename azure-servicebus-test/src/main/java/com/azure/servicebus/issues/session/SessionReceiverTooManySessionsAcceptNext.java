8e21ac8e9e9d805eebcbe4ed366f0e7e48718abc
Kun Liu <liuku@microsoft.com>
2023-01-31 17:27:09 +0800

    add SB session processor test

---
+++ azure-servicebus-test/src/main/java/com/azure/servicebus/issues/session/SessionReceiverTooManySessionsAcceptNext.java
@@ -0,0 +1,84 @@
+package com.azure.servicebus.issues.session;
+
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusReceiverAsyncClient;
+import com.azure.messaging.servicebus.ServiceBusSessionReceiverAsyncClient;
+import com.azure.messaging.servicebus.models.ServiceBusReceiveMode;
+import com.azure.servicebus.util.Credentials;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SessionReceiverTooManySessionsAcceptNext {
+
+    // 256 works, 257 will break the receiver
+    final static Integer SessionCount = 257;
+
+    public static void main(String[] args) throws InterruptedException {
+        SessionReceiverTooManySessionsAcceptNext sample = new SessionReceiverTooManySessionsAcceptNext();
+
+        final String sessionQueue = "sessionqueue";
+
+        // Create a receiver.
+        ServiceBusSessionReceiverAsyncClient sessionReceiver = new ServiceBusClientBuilder()
+                .connectionString(Credentials.serviceBusConnectionString)
+                .sessionReceiver()
+                .receiveMode(ServiceBusReceiveMode.PEEK_LOCK)
+                .queueName(sessionQueue)
+                .buildAsyncClient();
+
+        ArrayList<Disposable> subscriptions = new ArrayList<Disposable>();
+
+        for (Integer i = 0; i < SessionCount; i++) {
+            String sessionId = i.toString();
+            System.out.println("Receiving from Session " + sessionId);
+
+            Disposable sub = sample.run(sessionReceiver, sessionId);
+            subscriptions.add(sub);
+        }
+
+        System.out.println("All session receivers active. Press any key to exit.");
+
+        Scanner in = new Scanner(System.in);
+
+        String s = in.nextLine();
+
+        System.out.println("Disposing subscriptions ...");
+        for (var sub : subscriptions) {
+            sub.dispose();
+        }
+
+        System.out.println("Closing receiver ...");
+        sessionReceiver.close();
+
+        System.out.println("Bye.");
+    }
+
+    public Disposable run(ServiceBusSessionReceiverAsyncClient sessionReceiver, String sessionId) {
+
+        try {
+            Mono<ServiceBusReceiverAsyncClient> receiverMono = sessionReceiver.acceptNextSession();
+
+            System.out.println("-- Session " + sessionId + " accepted.");
+
+            Disposable subscription = Flux.usingWhen(receiverMono,
+                            receiver -> receiver.receiveMessages(),
+                            receiver -> Mono.fromRunnable(() -> receiver.close()))
+                    .subscribe(message -> {
+                        System.out.printf("Session: %s. Sequence #: %s. Contents: %s%n", message.getSessionId(),
+                                message.getSequenceNumber(), message.getBody());
+
+                    }, error -> {
+                        System.err.println("Error occurred: " + error);
+                    });
+
+            return subscription;
+        } catch (Exception ex) {
+            System.err.println("Error accepting session " + sessionId + ": " + ex);
+            return null;
+        }
+    }
+}
