4eafd0a483093b0ef729a9c307eaa01d03b577f3
Kun Liu <liuku@microsoft.com>
2023-02-16 10:29:40 +0800

    Update send batch messages test

--- azure-servicebus-test/src/main/java/com/azure/servicebus/common/SendBatchMessages.java
+++ azure-servicebus-test/src/main/java/com/azure/servicebus/common/SendBatchMessages.java
@@ -9,11 +9,13 @@ import com.azure.servicebus.util.Credentials;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class SendBatchMessages {
 
-    private static final int SEND_BATCH_NUMBER = 100;
-    private static final int BATCH_MESSAGE_NUMBER = 100;
+    private static final int SEND_BATCH_NUMBER = 1000;
+    private static final int BATCH_MESSAGE_NUMBER = 1000;
+    private static final int PAYLOAD_SIZE_IN_BYTES = 1024;
 
     public static void main(String[] args) {
         AmqpRetryOptions options = new AmqpRetryOptions();
@@ -26,11 +28,13 @@ public class SendBatchMessages {
                 .queueName(Credentials.serviceBusQueue)
                 .buildClient();
 
+        byte[] payload = new byte[PAYLOAD_SIZE_IN_BYTES];
+        (new Random()).nextBytes(payload);
 
         for (int i = 0; i < SEND_BATCH_NUMBER; i++) {
             List<ServiceBusMessage> messages = new ArrayList<>();
             for(int j = 0; j < BATCH_MESSAGE_NUMBER; j++){
-                messages.add(new ServiceBusMessage("Hello world").setMessageId(String.valueOf(i * BATCH_MESSAGE_NUMBER + j)));
+                messages.add(new ServiceBusMessage(payload).setMessageId(String.valueOf(i * BATCH_MESSAGE_NUMBER + j)));
             }
 
             sender.sendMessages(messages);
